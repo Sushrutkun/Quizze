4dda83e3ee92e4bdb46f4c8c1929988d67bf331e
Sushrut <sushrutathawale1509@gmail.com>
2023-10-24 11:41:10 +0530

    Create react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* The objective of this assignment is to create a language learning game that 
+          helps users improve their language proficiency through interactive exercises 
+          and activities. The game should include frontend UI components, backend 
+          logic for scoring, and a database to store user progress and language data */}
+      
+    </div>
+  );
+}
+
+export default App;
