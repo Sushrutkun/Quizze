8bb7e0c1e40afead1b314a89f1c0314b87d2598b
Sushrut <sushrutathawale1509@gmail.com>
2023-10-26 20:29:25 +0530

    using javascript to reset form after submit

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className=' bg-black text-white text-xl pl-8 pt-1 pb-2'>
       NavBar
     </div>
   )
--- client/src/components/QuizCard.js
+++ client/src/components/QuizCard.js
@@ -6,6 +6,7 @@ import { GrLinkNext, GrLinkPrevious } from 'react-icons/gr'
 
 const QuizCard = ({ page, setPage }) => {
   const [apiData, setApiData] = useState([]);
+
   const [vis, setVis] = useState('visible');
   const [ans, setAns] = useState(false);
   console.log(page);
@@ -21,8 +22,14 @@ const QuizCard = ({ page, setPage }) => {
   }, [page]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    // form.reset();
-    // e.reset();
+    let radio = document.getElementsByName("question");
+    console.log(radio);
+    for (let i = 0; i < radio.length; i++) {
+      // Do stuff
+      console.log(radio[i]);
+      radio[i].checked = false;
+    }
+    // radio.checked = false;
     setPage(page + 1);
     ans === true ? alert("**correct**") : alert("incorrect")
   }
@@ -33,7 +40,7 @@ const QuizCard = ({ page, setPage }) => {
       {
         apiData.map((data_entry) => (
           <div>
-            <h1 className='Quiz Category text-4xl text-center'>This is a {data_entry.category} Quizze</h1>
+            <h1 className='Quiz Category text-4xl text-center'>This is a {data_entry.category} Quizz</h1>
             <div className='box ml-40 mt-10 mr-40'>
               <div className='question border-2 rounded-2xl border-gray-800'>
                 <div className='flex justify-between'>
@@ -42,7 +49,7 @@ const QuizCard = ({ page, setPage }) => {
                     <h2>Question number {page} of 50</h2>
                   </div>
                   <div className='flex items-center gap-3 justify-start mr-4 mt-2 mb-2 text-md pl-1 pt-1 pb-1 pr-1 border-gray-500'>
-                    <div className= {`flex gap-1 items-center cursor-pointer hover:border-2 hover:rounded-lg border-gray-500 pl-1 pr-1 pt-1 pb-1 ${page===1 ? 'hidden':'visible'} `} style={{ visibility: vis }}
+                    <div className={`flex gap-1 items-center cursor-pointer hover:border-2 hover:rounded-lg border-gray-500 pl-1 pr-1 pt-1 pb-1 ${page === 1 ? 'hidden' : 'visible'} `} style={{ visibility: vis }}
                       onClick={() => {
                         page > 1 ? setPage(page - 1) : setPage(page);
                       }
@@ -51,7 +58,7 @@ const QuizCard = ({ page, setPage }) => {
                       <GrLinkPrevious />
                       <h2>Previous</h2>
                     </div>
-                    <div className={`flex gap-1 items-center cursor-pointer hover:border-2 hover:rounded-lg border-gray-500 pl-1 pr-1 pt-1 pb-1 ${page===50 ? 'hidden':'visible'}`} onClick={() => setPage(page + 1)}>
+                    <div className={`flex gap-1 items-center cursor-pointer hover:border-2 hover:rounded-lg border-gray-500 pl-1 pr-1 pt-1 pb-1 ${page === 50 ? 'hidden' : 'visible'}`} onClick={() => setPage(page + 1)}>
                       <h2>Skip</h2>
                       <GrLinkNext />
                     </div>
@@ -59,7 +66,7 @@ const QuizCard = ({ page, setPage }) => {
                 </div>
                 <div className='new_div ml-12 mt-2 mb-2 mr-12 flex flex-col flex-wrap '>
                   <h3 className='Head ml-4 mr-4 text-xl font-semibold'>{data_entry.question}</h3>
-                  <h5 className='Difficulty ml-4 text-base text-orange-600'>{data_entry.difficulty}</h5>
+                  <h5 className='Difficulty ml-4 text-base text-orange-600'>{data_entry.difficulty.toUpperCase()}</h5>
                 </div>
                 <div className='ml-12'>
                   <div className='ml-4 text-gray-600 text-lg'>
@@ -67,9 +74,9 @@ const QuizCard = ({ page, setPage }) => {
                   </div>
                   <div className='flex flex-col text-lg gap-2 ml-8 mr-96'>
                     <form id='myform'>
-                      <label onClick={() => { setAns(true); console.log(ans) }}>
+                      <label onClick={() => { setAns(true); console.log(ans) }} >
                         <div className=' hover:bg-zinc-300 cursor-pointer rounded-lg duration-200'>
-                          <input type='radio' name='question' class="w-3 h-3 mr-4 ml-2" />
+                          <input type='radio' id='1' name='question' class="w-3 h-3 mr-4 ml-2" />
                           {data_entry.correct_answer}
                         </div>
                       </label>
@@ -86,7 +93,7 @@ const QuizCard = ({ page, setPage }) => {
                       <div className=''>
                         <button type='reset' className='text-base text-red-400 mb-2 mt-1'>Clear Selection</button>
                         <div className='flex justify-end items-center'>
-                          <button type='submit' className=' text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2' onClick={handleSubmit}>Submit</button>
+                          <button type='submit' className=' text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2' onClick={handleSubmit}>Save & Next</button>
                         </div>
                       </div>
                     </form>
